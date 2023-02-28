a1c7ab2cacd09441f43cce6b03da58cad790e9ce
Araceli Fernandez <noe_3006@live.com.ar>
2023-02-28 18:06:16 -0300

    Pude crear una funtion en javascript, parece funcionar

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-function toggleClass(elem,className){
-	if (elem.className.indexOf(className) !== -1){
-		elem.className = elem.className.replace(className,'');
+function validateEmailAddress(email) {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
 	}
-	else{
-		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-	}
-	
-	return elem;
-}
-
-function toggleDisplay(elem){
-	const curDisplayStyle = elem.style.display;			
-				
-	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-		elem.style.display = 'block';
-	}
-	else{
-		elem.style.display = 'none';
-	}
-}
-
-
-function toggleMenuDisplay(e){
-	const dropdown = e.currentTarget.parentNode;
-	const menu = dropdown.querySelector('.menu');
-
-	toggleClass(menu,'hide');
-}
